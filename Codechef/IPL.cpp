23343d3ae4192f0dbf6123a9d54d61c44bc5ec8a
N <187439614+StriverV@users.noreply.github.com>
2025-04-16 20:52:05 +0600

    Add files via upload

---
+++ Codechef/IPL.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+#define Naba ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long 
+#define nl '\n'
+#define yes cout << "YES" << nl;
+#define no cout << "NO" << nl;
+using namespace std;
+
+int main()
+{
+    Naba;
+    int n;
+    cin >> n;
+
+    n >= 7? cout << "THALA" << nl: cout << "BOOM" << nl;
+
+    return 0;
+}
